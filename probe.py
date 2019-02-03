2ad9d8ffbfb15432dfc36d3dc6d585e651880e08
Forrest Thiessen <fthiess@gmail.com>
2019-02-03 09:08:08 -0800

    Separate sensors into a library

--- probe.py
+++ probe.py
@@ -2,9 +2,9 @@
 
 import config
 
-from smbus2 import SMBus
-from bmp280 import BMP280  # Temperature/pressure sensor
-import ltr559              # Light/proximity sensor
+# from smbus2 import SMBus
+# from bmp280 import BMP280  # Temperature/pressure sensor
+# import ltr559              # Light/proximity sensor
 
 from luma.core.interface.serial import i2c
 from luma.core.render import canvas
@@ -17,38 +17,7 @@ from Adafruit_IO import Client, Feed, RequestError
 import os
 import time
 
-
-class temp_sensor:
-    def __init__(self, probe_id, aio):
-        self.aio = aio
-        bus = SMBus(1)
-        self.temp_feed = self.aio.feeds('temperature-' + probe_id)
-        self.pressure_feed = self.aio.feeds('pressure-' + probe_id)
-        self.bmp280 = BMP280(i2c_dev=bus)
-    
-    def reading(self):
-        tempc = self.bmp280.get_temperature()
-        tempf = tempc * 9 / 5 + 32
-        pressure = self.bmp280.get_pressure()
-        
-        self.aio.send(self.temp_feed.key, tempf)
-        self.aio.send(self.pressure_feed.key, pressure)
-        
-        return tempf, pressure
-
-
-class light_sensor:
-    def __init__(self, probe_id, aio):
-        self.aio = aio
-        self.lux_feed = self.aio.feeds('light-' + probe_id)
-    
-    def reading(self):
-        lux = ltr559.get_lux()
-        prox = ltr559.get_proximity()
-
-        self.aio.send(self.lux_feed.key, lux)
-        
-        return lux, prox
+import sensors
 
 
 def make_font(name, size):
@@ -60,8 +29,8 @@ def make_font(name, size):
 aio = Client(config.adafruit_io_username, config.adafruit_io_key)
 
 # Instantiate sensor objects
-bmp = temp_sensor(config.probe_id, aio)
-ltr = light_sensor(config.probe_id, aio)
+bmp = sensors.temp_sensor(config.probe_id, aio)
+ltr = sensors.light_sensor(config.probe_id, aio)
 
 serial = i2c(port=1, address=0x3C)
 device = sh1106(serial_interface=serial,
---
+++ sensors.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+# Library with code to talk to sensors
+
+
+from smbus2 import SMBus
+from bmp280 import BMP280  # Temperature/pressure sensor
+import ltr559              # Light/proximity sensor
+
+#from Adafruit_IO import Client, Feed, RequestError
+
+
+class temp_sensor:
+    def __init__(self, probe_id, aio):
+        self.aio = aio
+        bus = SMBus(1)
+        self.temp_feed = self.aio.feeds('temperature-' + probe_id)
+        self.pressure_feed = self.aio.feeds('pressure-' + probe_id)
+        self.bmp280 = BMP280(i2c_dev=bus)
+    
+    def reading(self):
+        tempc = self.bmp280.get_temperature()
+        tempf = tempc * 9 / 5 + 32
+        pressure = self.bmp280.get_pressure()
+        
+        self.aio.send(self.temp_feed.key, tempf)
+        self.aio.send(self.pressure_feed.key, pressure)
+        
+        return tempf, pressure
+
+
+class light_sensor:
+    def __init__(self, probe_id, aio):
+        self.aio = aio
+        self.lux_feed = self.aio.feeds('light-' + probe_id)
+    
+    def reading(self):
+        lux = ltr559.get_lux()
+        prox = ltr559.get_proximity()
+
+        self.aio.send(self.lux_feed.key, lux)
+        
+        return lux, prox
