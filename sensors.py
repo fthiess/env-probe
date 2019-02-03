3b76dc19d8d495fba04d650df36766f035b2c632
fthiess <fthiess@gmail.com>
2019-02-02 16:49:51 -0800

    First attempt to objectify bmp280

--- sensors.py
+++ sensors.py
@@ -18,14 +18,28 @@ import os
 import time
 
 
+class env_sensor:
+    def __init__(self, probe_id, aio):
+        bus = SMBus(1)
+        self.feed = aio.feeds('temperature-' + probe_id)
+        self.bmp280 = BMP280(i2c_dev=bus)
+    
+    def reading(self):
+        tempc = self.bmp280.get_temperature()
+        tempf = tempc * 9 / 5 + 32
+        self.feed.send(self.feed.key, tempf)
+
+
 def make_font(name, size):
     font_path = os.path.abspath(os.path.join(
         os.path.dirname(__file__), 'fonts', name))
     return ImageFont.truetype(font_path, size)
 
+# Instantiate an Adafruit.IO object
+aio = Client(config.adafruit_io_username, config.adafruit_io_key)
 
-bus = SMBus(1)
-bmp280 = BMP280(i2c_dev=bus)
+# Instantiate a env sensor object
+bmp280 = env_sensor(config.probe_id, aio)
 
 serial = i2c(port=1, address=0x3C)
 device = sh1106(serial_interface=serial,
@@ -35,29 +49,24 @@ device = sh1106(serial_interface=serial,
 
 font = make_font("DroidSansMono.ttf", 20)
 
-aio = Client(config.adafruit_io_username, config.adafruit_io_key)
 
-temperature_feed = aio.feeds('temperature-'+config.probe_id)
-pressure_feed = aio.feeds('pressure-'+config.probe_id)
-light_feed = aio.feeds('light-'+config.probe_id)
+pressure_feed = aio.feeds('pressure- '+ config.probe_id)
+light_feed = aio.feeds('light-' + config.probe_id)
 
 
 while True:
-    tempc = bmp280.get_temperature()
-    tempf = tempc * 9 / 5 + 32
-    pressure = bmp280.get_pressure()
+    temp = bmp280.reading()
 
     lux = ltr559.get_lux()
     prox = ltr559.get_proximity()
 
     with canvas(device) as draw:
-        draw.text((0, 15), 'T {:>8.2f}'.format(tempf), font=font, fill="white")
+        draw.text((0, 15), 'T {:>8.2f}'.format(temp), font=font, fill="white")
         draw.text((0, 50), 'P {:>8.2f}'.format(pressure), font=font, fill="white")
         draw.text((0, 85), 'L {:>8.2f}'.format(lux), font=font, fill="white")
 
-    print("T: {:05.2f}*F P: {:08.2f}hPa L: {:08.2f} Px: {}".format(tempf, pressure, lux, prox))
+    print("T: {:05.2f}*F P: {:08.2f}hPa L: {:08.2f} Px: {}".format(temp, pressure, lux, prox))
 
-    aio.send(temperature_feed.key, tempf)
     aio.send(pressure_feed.key, pressure)
     aio.send(light_feed.key, lux)
 
