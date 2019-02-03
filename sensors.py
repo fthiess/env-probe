94d6fffef056a9a3a69551f57a8146cc61b25d92
Forrest Thiessen <fthiess@gmail.com>
2019-02-02 17:26:47 -0800

    Working bmp280 object

--- sensors.py
+++ sensors.py
@@ -20,14 +20,21 @@ import time
 
 class env_sensor:
     def __init__(self, probe_id, aio):
+        self.aio = aio
         bus = SMBus(1)
-        self.feed = aio.feeds('temperature-' + probe_id)
+        self.temp_feed = self.aio.feeds('temperature-' + probe_id)
+        self.pressure_feed = self.aio.feeds('pressure-' + probe_id)
         self.bmp280 = BMP280(i2c_dev=bus)
     
     def reading(self):
         tempc = self.bmp280.get_temperature()
         tempf = tempc * 9 / 5 + 32
-        self.feed.send(self.feed.key, tempf)
+        pressure = self.bmp280.get_pressure()
+        
+        self.aio.send(self.temp_feed.key, tempf)
+        self.aio.send(self.pressure_feed.key, pressure)
+        
+        return tempf,pressure
 
 
 def make_font(name, size):
@@ -50,12 +57,12 @@ device = sh1106(serial_interface=serial,
 font = make_font("DroidSansMono.ttf", 20)
 
 
-pressure_feed = aio.feeds('pressure- '+ config.probe_id)
+pressure_feed = aio.feeds('pressure-'+ config.probe_id)
 light_feed = aio.feeds('light-' + config.probe_id)
 
 
 while True:
-    temp = bmp280.reading()
+    temp,pressure = bmp280.reading()
 
     lux = ltr559.get_lux()
     prox = ltr559.get_proximity()
@@ -67,7 +74,6 @@ while True:
 
     print("T: {:05.2f}*F P: {:08.2f}hPa L: {:08.2f} Px: {}".format(temp, pressure, lux, prox))
 
-    aio.send(pressure_feed.key, pressure)
     aio.send(light_feed.key, lux)
 
     time.sleep(60)
