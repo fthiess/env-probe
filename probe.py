b6e7d6edb6e3e6b14d9c8048d320981b5b1c70ed
Forrest Thiessen <fthiess@gmail.com>
2019-02-17 17:56:08 -0800

    Disabled OLED display

--- probe.py
+++ probe.py
@@ -18,15 +18,15 @@ bmp = sensors.temp_sensor(config.probe_id, aio)
 ltr = sensors.light_sensor(config.probe_id, aio)
 
 # Instantiate display
-oled = display.oled()
+#oled = display.oled()
 
 
 while True:
     temp, pressure = bmp.read()
     lux, prox = ltr.read()
 
-    oled.show(temp=temp, pressure=pressure, lux=lux)
-    
-    print("T: {:>5.2f}*F P: {:>8.2f}hPa L: {:>6.0f} Px: {:>5.0f}".format(temp, pressure, lux, prox))
+#    oled.show(temp=temp, pressure=pressure, lux=lux)
+#    
+#    print("T: {:>5.2f}*F P: {:>8.2f}hPa L: {:>6.0f} Px: {:>5.0f}".format(temp, pressure, lux, prox))
 
     time.sleep(config.sleep_between_samples)
