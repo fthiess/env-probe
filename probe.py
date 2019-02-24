6e2981fb5116aa2dd8a3cbdbd079f134548b969f
Forrest Thiessen <fthiess@gmail.com>
2019-02-23 17:32:11 -0800

    First addition of exception handling

--- probe.py
+++ probe.py
@@ -5,6 +5,7 @@ import config
 from Adafruit_IO import Client, Feed, RequestError
 
 import time
+import datetime
 
 import libraries.sensors as sensors
 import libraries.display as display
@@ -12,24 +13,46 @@ import libraries.display as display
 
 # Instantiate an Adafruit.IO object
 aio = Client(config.adafruit_io_username, config.adafruit_io_key)
+print("Adafruit.io initialized")
 
 # Instantiate sensor objects
 bmp = sensors.temp_sensor(config.probe_id, aio)
+print("BMP sensor initialized")
+
 ltr = sensors.light_sensor(config.probe_id, aio)
+print("Light sensor initialized")
+
 
 # Instantiate display
 #oled = display.oled()
 
 
 while True:
-    temp = bmp.read()
-    bmp.push(temp)
-    
-    lux, prox = ltr.read()
-    ltr.push(lux)
+    try:
+        temp = -1
+        temp = bmp.read()
+        try:
+            bmp.push(temp)
+        except:
+            print("Excepting pushing temp to Adafruit.io")
+    except:
+        print("Temp read exception, not pushing")
+
+    try:
+        lux = -1
+        prox = -1
+        lux, prox = ltr.read()
+        try:
+            ltr.push(lux)
+        except:
+            print("Excepting pushing lux to Adafruit.io")
+    except:
+        print("Lux read exception, not pushing")
+        
 
 #    oled.show(temp=temp, pressure=pressure, lux=lux)
     
-    print("T: {:>5.2f}*F L: {:>6.0f} Px: {:>5.0f}".format(temp, lux, prox))
+    print(datetime.datetime.now()," temp=",temp," lux=",lux)
+
 
     time.sleep(config.sleep_between_samples)
